42475069075850239447db45bfb1d0bfa3896945
Your Name <you@example.com>
2025-02-10 23:13:52 -0800

    had to rewrite the code becasue alot of it wasnt working, also added everything

--- comp003a.lecture activity 5-1/Program.cs
+++ comp003a.lecture activity 5-1/Program.cs
@@ -1,34 +1,70 @@
-﻿using System.Xml.Serialization;
+﻿// Author: Roman
+// Course: COMP003A
+// Faculty: Jonathan Cruz
+// Purpose: Demonstrate the creation and usage of classes in C#
+
+using System.Security.Cryptography.X509Certificates;
 
 namespace comp003a.lecture_activity_5_1
 {
-    internal class car
+    internal class Car
     {
-        //fields 
+        //fields
+        private static int carCount = 0;
         public string Make;
         public string Model;
         public int Year;
 
-        ///<summary>
-        ///Displays the car information
-        ///</summary>
 
+        public string make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            carCount++;
+        }
         public void DisplayInfo()
         {
-            Console.WriteLine($"Car: {Year} {Model} {Year}");
+            Console.WriteLine($"Car: {Year} {Make} {Model}");
+        }
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine($"Total cars created: {carCount}");
         }
     }
 
     internal class program
     {
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
-            car mycar = new car();
-            mycar.Make = "Toyota";
-            mycar.Model = "Supra";
-            mycar.Year = 2024;
+            Car myCar = new Car("Honda", "Civic", 2021);
+            myCar.DisplayInfo();
 
-            mycar.DisplayInfo();
+            myCar.Make = "toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2020;
+            myCar.DisplayInfo();
+
+            Car myCar2 = new Car("Ford", "mustang", 2020);
+            Car myCar3 = new Car("chevrolet", "camero", 2021);
+            Car.DisplayCarCount();
+
         }
     }
 }
