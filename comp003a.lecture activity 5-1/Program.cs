52e596decbe2b151acffb4ba40c5dcd143143613
Your Name <you@example.com>
2025-02-10 22:21:01 -0800

    Add project files.

---
+++ comp003a.lecture activity 5-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace comp003a.lecture_activity_5_1
+{
+    internal class car
+    {
+        //fields 
+        public string Make;
+        public string Model;
+        public int Year;
+
+        ///<summary>
+        ///Displays the car information
+        ///</summary>
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Car: {Year} {Model} {Year}");
+        }
+    }
+
+    internal class program
+    {
+        static void Main( string[] args )
+        {
+
+        }
+    }
+}
